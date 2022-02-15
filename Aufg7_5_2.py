b891aa703216ffb2fb7339b221dbdd76f78f698a
F. A <44412200+faymaz@users.noreply.github.com>
2022-02-15 16:53:14 +0100

    Add files via upload

--- Aufg7_5_2.py
+++ Aufg7_5_2.py
@@ -4,14 +4,14 @@ quiz_erdkunde_3 = ["Hauptstadt von der USA ", "Washington"]
 quiz_erdkunde_4 = ["Hauptstadt von China ", "Peking"]
 quiz_erdkunde_5 = ["Hauptstadt von Russland ", "Moskau"]
 
-quiz_math_1 = ["25 + 30 = ", 55]
-quiz_math_2 = ["8 x 3 = ", 24]
+quiz_math_1 = ["225 + 32 = ", 55]
+quiz_math_2 = ["8 x 4 = ", 24]
 quiz_math_3 = ["12 + 26 = ", 38]
 quiz_math_4 = ["3 x 15 = ", 45]
 quiz_math_5 = ["48 - 14 = ", 34]
 
 b=0
-print(" Hallo Baha!!!, \n Du hast 5 Fragen zu beantworten. Wenn du mindestens 3 richtig beantwortest, bekommst du als Belohnung Handyspielzeit. Wenn du 2 oder 1 richtig beantwortest, dann darfst du nur mit MIR !!! Minecraft spielen.\n\n\t")
+print(" Hallo!, \n Du hast 10 Fragen zu beantworten. Wenn du mindestens 8 richtig beantwortest, bekommst du als Belohnung Handyspielzeit. Wenn du 5 oder 7 richtig beantwortest, dann darfst du nur Minecraft spielen.\n\n\t")
 
 a = eval(input(quiz_math_1[0]))
 if (quiz_math_1[1] == a):
@@ -92,7 +92,9 @@ if (b >= 1) and (b <= 2):
     print("\n",b,", Punkt Viele Fehler: weitere Übung erforderlich!")
 elif (b >= 3) and (b <= 4):
     print("\n",b,", Punkte Gute Leistung!")
-elif b == 5:
+elif (b >= 5) and (b>=7):
+    print("\n",b,", Sehr Gut!!!")
+elif b >= 8:
     print("\n",b,", Punkte Super! Alle Aufgaben richtig gelöst!!!")
 else:
     print("\n",b,", Punkt Dringend Nachhilfe benötigt!")
